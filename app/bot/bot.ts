48ce18039682bcf7ff20b01e112e70666328d073
AlexandreRault <alexandre.rault@polymtl.ca>
2018-10-06 16:09:11 -0400

    functions for tile search

--- app/bot/bot.ts
+++ app/bot/bot.ts
@@ -30,7 +30,17 @@ export class Bot {
      */
     public executeTurn(map: Map, visiblePlayers: Player[]): string {
         // Determine what action you want to take.
+        if (this.playerInfo.CarriedResources === this.playerInfo.CarryingCapacity) {
+            this.chosenTile = map.getTileAt(this.playerInfo.HouseLocation);
+            this.xMovement = this.playerInfo.Position.x - this.playerInfo.HouseLocation.x;
+            this.yMovement = this.playerInfo.Position.y - this.playerInfo.HouseLocation.y;
+            this.isOccupied = true;
+        } else if (this.chosenTile !== null && map.getTileAt(this.chosenTile.Position).TileType !== TileContent.Resource ) {
+            console.log(3);
+            this.isOccupied = false;
+        }
         if (!this.isOccupied) {
+            console.log(4);
             const array = new Array;
             for (let i = -map.visibleDistance; i < map.visibleDistance; i++) {
                 for (let j = -map.visibleDistance; j < map.visibleDistance; j++) {
@@ -51,29 +61,27 @@ export class Bot {
         }
         // Player is on the left
         if (this.xMovement < -1) {
-            console.log(1);
             this.xMovement++;
             return AIHelper.createMoveAction(new Point(1, 0));
         }
         // Player is on the right
         if (this.xMovement > 1) {
-            console.log(2);
             this.xMovement--;
             return AIHelper.createMoveAction(new Point(-1, 0));
         }
         // Player is above
         if (this.yMovement < 0) {
-            console.log(3);
             this.yMovement++;
             return AIHelper.createMoveAction(new Point(0, 1));
         }
         // Player is under
         if (this.yMovement > 0) {
-            console.log(4);
             this.yMovement++;
             return AIHelper.createMoveAction(new Point(0, -1));
         }
-
+        if (this.chosenTile.TileType === TileContent.House) {
+            return AIHelper.createMoveAction(new Point(this.playerInfo.HouseLocation.x - this.playerInfo.Position.x, 0));
+        }
         return AIHelper.createCollectAction(new Point(-this.xMovement, this.yMovement));
     }
 
@@ -83,5 +91,44 @@ export class Bot {
      */
     public afterTurn(): void {
         return;
+    }
+
+    private findClosestRessource(map: Map): Point {
+        const array = new Array;
+            for (let i = -map.visibleDistance; i < map.visibleDistance; i++) {
+                for (let j = -map.visibleDistance; j < map.visibleDistance; j++) {
+                    const tile: Tile = map.getTileAt(new Point(this.playerInfo.Position.x + i, this.playerInfo.Position.y + j));
+                    if (tile.TileType === TileContent.Resource) {
+                        array.push(tile);
+                    }
+                }
+            }
+            array.sort((tile1: Tile, tile2: Tile) => {
+                return Point.distance(new Point(10, 10), tile1.Position)
+                    - Point.distance(new Point(10, 10), tile2.Position);
+            });
+            if (array.length === 0) {
+                throw new Error('No ressources found in the visible area');
+            }
+            return array[0].Position;
+    }
+
+    private findHomeClosestPoint(map: Map): Point {
+        const point: Point = new Point(0, 0);
+        point.x = this.playerInfo.HouseLocation.x - this.playerInfo.Position.x + 10;
+        point.y = this.playerInfo.HouseLocation.y - this.playerInfo.Position.y + 10;
+        if (point.x > 20) {
+            point.x = 20;
+        }
+        if (point.x < 0) {
+            point.x = 0;
+        }
+        if (point.y > 20) {
+            point.y = 20;
+        }
+        if (point.y < 0) {
+            point.x = 0;
+        }
+        return point;
     }
 }
