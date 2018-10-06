8a05af588134ea1a369ab48478e65f16455d6152
AlexandreRault <alexandre.rault@polymtl.ca>
2018-10-06 14:23:10 -0400

    Recolting not much

--- app/bot/bot.ts
+++ app/bot/bot.ts
@@ -8,6 +8,8 @@ export class Bot {
     protected playerInfo: Player;
     private isOccupied: boolean = false;
     private chosenTile: Tile = null;
+    private xMovement: number = 0;
+    private yMovement: number = 0;
 
     /**
      * Gets called before ExecuteTurn. This is where you get your bot's state.
@@ -27,51 +29,52 @@ export class Bot {
      */
     public executeTurn(map: Map, visiblePlayers: Player[]): string {
         // Determine what action you want to take.
-        console.log(100);
         if (!this.isOccupied) {
-            console.log(101);
             let array = new Array;
-            console.log(102);
             for(let i = -map.visibleDistance; i < map.visibleDistance; i++) {
-                console.log(103);
                 for(let j = -map.visibleDistance; j < map.visibleDistance; j++) {
                     let tile : Tile = map.getTileAt(new Point(this.playerInfo.Position.x + i,                                                             this.playerInfo.Position.y + j));
-                    console.log(tile);
                     if (tile.TileType === TileContent.Resource)
                         array.push(tile);
                 }
             }
             console.log(array);
             array.sort((tile1 : Tile, tile2: Tile) => {
-                return Point.distance(this.playerInfo.Position, tile1.Position) 
-                    - Point.distance(this.playerInfo.Position, tile2.Position);
+                return Point.distance(new Point(10,10), tile1.Position) 
+                    - Point.distance(new Point(10,10), tile2.Position);
 
             });
             this.chosenTile = array[0];
+            this.xMovement = 10 - this.chosenTile.Position.x;
+            this.yMovement = 10 - this.chosenTile.Position.y;
             this.isOccupied = true;
         }
         // Player is on the left
-        if (this.playerInfo.Position.x < this.chosenTile.Position.x - 1) {
+        if (this.xMovement < -1) {
             console.log(1);
+            this.xMovement++;
             return AIHelper.createMoveAction(new Point(1, 0));
         }
         //Player is on the right
-        if (this.playerInfo.Position.x > this.chosenTile.Position.x + 1) {
-            console.log(this.playerInfo.Position.x + " " + this.chosenTile.Position.x);
+        if (this.xMovement > 1) {
+            console.log(2);
+            this.xMovement--;
             return AIHelper.createMoveAction(new Point(-1, 0));
         }
         //Player is above
-        if (this.playerInfo.Position.y < this.chosenTile.Position.y) {
+        if (this.yMovement < 0) {
             console.log(3);
+            this.yMovement++;
             return AIHelper.createMoveAction(new Point(0, 1));
         }
         //Player is under
-        if (this.playerInfo.Position.y > this.chosenTile.Position.y) {
+        if (this.yMovement > 0) {
             console.log(4);
+            this.yMovement++;
             return AIHelper.createMoveAction(new Point(0, -1));
         }
 
-        return AIHelper.createCollectAction(new Point(this.playerInfo.Position.x - this.chosenTile.Position.x, this.playerInfo.Position.y - this.chosenTile.Position.y));
+        return AIHelper.createCollectAction(new Point(-this.xMovement, this.yMovement));
     }
 
     /**
