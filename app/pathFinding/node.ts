0903b9defd9cda7b51dd538f2b728da5f3f87b35
GenevieveBock1 <genevieve.bock@polymtl.ca>
2018-10-06 12:58:10 -0400

    gfg

--- app/pathFinding/node.ts
+++ app/pathFinding/node.ts
@@ -8,13 +8,15 @@ export class Node {
     public f: number;
     public g: number;
     public h: number;
+    public vh: number;
     public previous: Node;
 
-    public constructor(x: number, y: number, previous: Node = null, f: number = 0, g: number = 0, h: number = 0) {
+    public constructor(x: number, y: number, previous: Node = null, f: number = 0, g: number = 0, h: number = 0, vh: number = 0) {
         this.point = new Point(x, y);
         this.f = f;
         this.g = g;
         this.h = h;
+        this.vh = vh;
         this.previous = previous;
     }
 
