77c45806e03e1ac4d34bc4ddde654606903ff44f
JeHuppe <jeremie123go@gmail.com>
2018-10-06 14:34:56 -0400

    debugging

--- app/bot/bot.ts
+++ app/bot/bot.ts
@@ -2,10 +2,11 @@ import { AIHelper } from '../helper/aiHelper';
 import { Player } from '../helper/interfaces';
 import { Map } from '../helper/map';
 import { Point } from '../helper/point';
+import { PatheFinder } from '../pathFinding/pahtFinder';
 
 export class Bot {
     protected playerInfo: Player;
-
+    private pathFinder: PatheFinder = new PatheFinder();
     /**
      * Gets called before ExecuteTurn. This is where you get your bot's state.
      * @param  {Player} playerInfo Your bot's current state.
@@ -13,6 +14,11 @@ export class Bot {
      */
     public beforeTurn(playerInfo: Player): void {
         this.playerInfo = playerInfo;
+
+
+
+
+
     }
     /**
      * This is where you decide what action to take.
@@ -22,12 +28,17 @@ export class Bot {
      */
     public executeTurn(map: Map, visiblePlayers: Player[]): string {
         // Determine what action you want to take.
-        return AIHelper.createMoveAction(new Point(0, 1));
+        console.log(this.playerInfo.Position);
+
+        console.log(this.pathFinder.getShortestPath(this.playerInfo, map, this.playerInfo.Position, new Point(9, 39)));
+        return ''; // AIHelper.createMoveAction(new Point(0, 1));
     }
 
     /**
      * Gets called after executeTurn
      * @returns void
      */
-    public afterTurn(): void { }
+    public afterTurn(): void {
+        console.log('afterTurn');
+     }
 }
--- app/pathFinding/pahtFinder.ts
+++ app/pathFinding/pahtFinder.ts
@@ -8,17 +8,14 @@ import { Node } from './node';
 import { POINT_CONVERSION_COMPRESSED } from 'constants';
 
 export class PatheFinder {
-
-    public constructor(private player: Player) {}
-
     // An educated guess of how far it is between two points
     public heuristic(a: Point, b: Point): number {
         return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
     }
 
-    public nbOfTurns(map: Map, a: Node, b: Node): number {
+    public nbOfTurns(player: Player, map: Map, a: Node, b: Node): number {
         if (map.getTileAt(b.point) === TileContent.Wall) {
-            return this.nbOfTurnsToKill(5, this.player.AttackPower);
+            return this.nbOfTurnsToKill(5, player.AttackPower);
         }
         return this.heuristic(a.point, b.point);
     }
@@ -52,7 +49,7 @@ export class PatheFinder {
     // returns 0 if search ongoing
     // returns 1 if goal reached
     // returns -1 if no solution
-    public findShortestPath(newMap: Map, start: Point, end: Point) {
+    public getShortestPath(player: Player, map: Map, start: Point, end: Point) {
         const openSet: Node[] = [];
         const startingNode = new Node(start.x, start.y, null, 0, 0, this.heuristic(start, end), Point.distance(start, end));
         openSet.push(startingNode);
@@ -60,18 +57,26 @@ export class PatheFinder {
         const closedSet: Node[] = [];
         let lastCheckedNode: Node = startingNode;
 
-        if (openSet.length > 0) {
-
+        while (openSet.length > 0) {
+            console.log('1');
             // Best next option
             let winner = 0;
             for (let i = 1; i < openSet.length; i++) {
+                console.log('2');
+
                 if (openSet[i].f < openSet[winner].f) {
+                    console.log('3');
+
                     winner = i;
                 }
                 // if we have a tie according to the standard heuristic
                 if (openSet[i].f === openSet[winner].f) {
+                    console.log('4');
+
                     // Prefer to explore options with longer known paths (closer to goal)
                     if (openSet[i].g > openSet[winner].g) {
+                        console.log('5');
+
                         winner = i;
                     }
                     // if we're using Manhattan distances then also break ties
@@ -83,15 +88,21 @@ export class PatheFinder {
                     // planning natural paths.
                     if (openSet[i].g === openSet[winner].g &&
                         openSet[i].vh < openSet[winner].vh) {
+                            console.log('6');
+
                         winner = i;
                     }
                 }
             }
+
             const current = openSet[winner];
             lastCheckedNode = current;
+            console.log('7');
 
             // Did I finish?
             if (Point.Equals(current.point, end)) {
+                console.log('8');
+
                 let actualNode = current;
                 const path: Point[] = [];
                 path.push(current.point);
@@ -111,17 +122,23 @@ export class PatheFinder {
 
             // Check all the neighbors
             const neighbors = current.getNeighbors();
+            console.log('9');
 
             for (let i = 0; i < neighbors.length; i++) {
                 const neighbor = neighbors[i];
+                console.log('10');
 
                 // Valid next spot?
                 if (!this.containsObject(closedSet, neighbor)) {
                     // Is this a better path than before?
-                    const tempG = current.g + this.heuristic(neighbor.point, current.point);
+                    console.log('11');
+
+                    const tempG = current.g + this.nbOfTurns(player, map, neighbor, current);
 
                     // Is this a better path than before?
-                    if (!_.contains(openSet, neighbor)) {
+                    if (!this.containsObject(openSet, neighbor)) {
+                        console.log('12');
+
                         openSet.push(neighbor);
                     } else if (tempG >= neighbor.g) {
                         // No, it's not a better path
@@ -137,8 +154,6 @@ export class PatheFinder {
                 }
 
             }
-            return null;
-        } else {
             return null;
         }
     }
